63b0faf6ef3f0ae85e657c5d36df0e4a7c6e2b1e
nimuy99 <nimuy99@gmail.com>
2024-11-08 23:57:44 +0900

    fix: 상세정보 조회 - 평점 평균, 제보 업소 수정

--- src/main/java/com/example/alddeul_babsang/repository/ReviewRepository.java
+++ src/main/java/com/example/alddeul_babsang/repository/ReviewRepository.java
@@ -2,6 +2,10 @@ package com.example.alddeul_babsang.repository;
 
 import com.example.alddeul_babsang.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("SELECT AVG(r.star_rating) FROM Review r WHERE r.store.id = :storeId")
+    Float findAverageRatingByStoreId(@Param("storeId") Long storeId);
 }
