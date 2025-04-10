f3d0da737c0aa35f7f0eb6d75467c2a63a535ca4
nimuy99 <nimuy99@gmail.com>
2025-04-10 14:23:35 +0900

    feat: 컨트롤러 태그 추가

--- src/main/java/com/example/alddeul_babsang/web/controller/TestController.java
+++ src/main/java/com/example/alddeul_babsang/web/controller/TestController.java
@@ -4,11 +4,13 @@ import com.example.alddeul_babsang.apiPayload.ApiResponse;
 import com.example.alddeul_babsang.service.CoordinatesService;
 import com.example.alddeul_babsang.web.dto.StoreDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "${swagger.tag.test}", description = "테스트 관련 APIs (백엔드만 사용할 것)")
 @RestController("/test")
 @RequiredArgsConstructor
 public class TestController {
